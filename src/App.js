54e5771424dd3a91e113df07d003cffc1a569fa7
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-07-08 09:04:29 +0530

    adding app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import LoginSignUp from "./pages/login-signup/LoginSignUp";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <Router>
-      <Route path="/" component={LoginSignUp}></Route>
+      <Route exact path="/bookstore" component={LoginSignUp}></Route>
+      <Route exact path="/bookstore/home" component={Home}></Route>
     </Router>
   );
 }
