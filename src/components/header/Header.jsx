c0ae4abc30f56f7647ec441897e18c4cb1bb6509
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-07-08 21:26:42 +0530

    feat: added badge for cart to represent count of items in cart

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,8 +3,9 @@ import SearchIcon from "@material-ui/icons/Search";
 import CartIcon from "@material-ui/icons/ShoppingCartOutlined";
 import "./Header.css";
 import book from "./education.svg";
+import { Badge } from "@material-ui/core";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <div>
       <header className="home-header">
@@ -20,9 +21,14 @@ export default function Header() {
             placeholder="Search"
           ></input>
         </div>
-        <div className="header-cart-icon">
+        <div
+          className="header-cart-icon"
+          // onClick={openCart}
+        >
           <p className="cart-tag">Cart</p>
-          <CartIcon />
+          <Badge badgeContent={props.count} color="secondary">
+            <CartIcon />
+          </Badge>
         </div>
       </header>
     </div>
