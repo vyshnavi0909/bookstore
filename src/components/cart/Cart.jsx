89ff03e310c887cc7521ac7851395c0f7702c9e1
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-07-09 09:31:29 +0530

    feat: added cart page sample

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,27 +1,41 @@
 // import { Card } from "@material-ui/core";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import UserServices from "../../services/UserServices";
+import Footer from "../footer/Footer";
+import Header from "../header/Header";
 
 const services = new UserServices();
 export default function Cart() {
-  const [cartBooks, setCartBooks] = useState("");
+  const [cartBooks, setCartBooks] = useState();
+  const [count, setCount] = useState();
   const getCartItems = () => {
     services
       .getFromCart()
       .then((res) => {
         console.log(res);
-        setCartBooks(res.result.product_id);
+        let cartcount = res.data.result.length;
+        setCount(cartcount);
+        setCartBooks(res.data.result);
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
+  useEffect(() => {
+    getCartItems();
+  }, []);
+
   return (
     <div>
-      {cartBooks.map((book, index) => (
-        <h5>{book.bookName}</h5>
-      ))}
+      <Header count={count} />
+      <div className="cart-container">
+        {cartBooks.map((book, index) => (
+          <h5>{book.product_id.bookName}</h5>
+        ))}
+      </div>
+
+      <Footer />
     </div>
   );
 }
